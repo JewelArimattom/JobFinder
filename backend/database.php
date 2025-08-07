a8d31e1ddd14e83ea021b515158f5b3fdcfc8793
Emil <emilsaji04@gmail.com>
2025-08-07 23:56:49 +0530

    Path changed, refactured databse connection logic

--- backend/database.php
+++ backend/database.php
@@ -1,21 +1,16 @@
 <?php
 // --- DATABASE CONFIGURATION ---
-$servername = "127.0.0.1";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "jobfinder";
+$dbname = "careerbridge";
 
 // Enable error reporting for debugging
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-// Create database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 // Set the timezone to India Standard Time
 date_default_timezone_set('Asia/Kolkata');
+
+// Log that this file was included
+error_log("Database configuration loaded from: " . __FILE__);
 ?>
